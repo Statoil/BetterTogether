0b8f39628ee397cb110847c84882e95fd4f281c1
esog <esog@statoil.com>
2018-07-06 08:35:01 +0200

    Remove permission

--- app/src/main/java/com/BetterTogether/app/Overview.java
+++ app/src/main/java/com/BetterTogether/app/Overview.java
@@ -1,27 +1,17 @@
 package com.BetterTogether.app;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.BetterTogether.app.adapters.TabAdapter;
 
-import DB.DatabaseThreadHandler;
-import DB.SQLiteDB;
-import JSONReader.ParsedPerson;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
 
 public class Overview extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_READ_EXTERNAL = 1;
+   // private static final int MY_PERMISSIONS_READ_EXTERNAL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +22,12 @@ public class Overview extends AppCompatActivity {
         ActionBar bar = getSupportActionBar();
         bar.hide();
 
-        if (!(ContextCompat.checkSelfPermission(getApplicationContext(),
+       /* if (!(ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)){
             ActivityCompat.requestPermissions(this,
                     new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
                     MY_PERMISSIONS_READ_EXTERNAL);
-        }
+        }*/
 
             TabAdapter tabAdapter = new TabAdapter
                     (getSupportFragmentManager(), new UserListFragment(), new GraphFragment());
@@ -48,20 +38,6 @@ public class Overview extends AppCompatActivity {
         tabLayout.bringToFront();
         tabLayout.setupWithViewPager(viewPager);
     }
-
-    private void testAddToJSONFile() {
-        ParsedPerson p = new ParsedPerson();
-        p.setUsername("json");
-        p.setFirstname("test");
-        p.setLastname("write");
-    }
-
-    /*
-    private void checkDB() {
-        handler.getThreshold(RewardType.CAKE)
-                .subscribe(reward -> Log.d("DATABASE-REWARD-CAKE-50"
-                , Integer.toString(reward)));
-    }*/
 
 
 }
