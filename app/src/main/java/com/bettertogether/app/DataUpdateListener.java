bd475f48eff6285abc986d21a5276d86f0dd911c
esog <esog@statoil.com>
2018-08-02 08:48:18 +0200

    Refactor package names
    Change to lower-case

---
+++ app/src/main/java/com/bettertogether/app/DataUpdateListener.java
@@ -0,0 +1,16 @@
+package com.bettertogether.app;
+
+import db.RewardType;
+
+public interface DataUpdateListener {
+
+    void tokenReceived(String token);
+
+    void responseError(int code, String message);
+
+    void updateGrid();
+
+    void updateStatus();
+
+    void useReward(RewardType type);
+}
